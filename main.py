d97f5c207bb77eb98134a6102817706cc5f3f358
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-10-27 15:26:30 +0200

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print(45)
+print("stefan, is met een web dev bezig")
+print('Hello', 'how are you?')
+x = ('apple', 'banana', 'cherry')
+print(x)
+print('apple', 'banana', 'cherry')
